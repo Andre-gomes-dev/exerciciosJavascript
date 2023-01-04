95354c249db4b29c2d39555e3e8be17331328305
andre-gomes <gomesandre749@gmail.com>
2023-01-04 12:13:29 -0300

    adições do exercicio, finalizando...

--- verificadorIdade/script.js
+++ verificadorIdade/script.js
@@ -18,26 +18,35 @@ function verificar() {
       genero = "homem";
       if (idade >= 0 && idade < 10) {
         //kid
+        img.setAttribute("src", "images/babyMen.jpg");
       } else if (idade < 21) {
         //young
+        img.setAttribute("src", "images/youngMen.jpg");
       } else if (idade < 60) {
         //adult
+        img.setAttribute("src", "images/adultMen.jpg");
       } else {
         //old
+        img.setAttribute("src", "images/oldMen.jpg");
       }
     } else if (formgen[1].checked) {
       genero = "mulher";
 
       if (idade >= 0 && idade < 10) {
         //kid
+        img.setAttribute("src", "images/babyWomen.jpg");
       } else if (idade < 21) {
         //young
+        img.setAttribute("src", "images/youngWomen.jpg");
       } else if (idade < 60) {
         //adult
+        img.setAttribute("src", "images/adultWomen.jpg");
       } else {
         //old
+        img.setAttribute("src", "images/oldWomen.jpg");
       }
     }
-    res.innerHTML = `Foi Detectado ${genero} com ${idade}anos de idade`;
+    res.innerHTML = `Foi Detectado ${genero} com ${idade} anos de idade`;
+    res.appendChild(img);
   }
 }
